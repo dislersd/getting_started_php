cb87a0db967bd68cd70a9d7aa205a5cb898afe67
dislersd <dislersd@gmail.com>
2020-04-23 16:13:31 -0700

    calculator updates

--- calculator.php
+++ calculator.php
@@ -6,14 +6,30 @@
   <title>Document</title>
 </head>
 <body>
-  <form action="calculator.php" method="get">
-    <input type="number" name="num1">
-    <br>
-    <input type="number" name="num2">
-    <br>
+  <form action="calculator.php" method="post">
+    Number 1: <input type="number" name="num1"> <br>
+    Operator: <input type="text" name="op"> <br>
+    Number 2: <input type="number" name="num2"> <br>
     <input type="submit">
   </form>
 
-  Answer: <?php echo $_GET["num1"] + $_GET["num2"]?>
+  <?php 
+    $num1 = $_POST["num1"];
+    $num2 = $_POST["num2"];
+    $op = $_POST["op"];
+
+    if ($op == "+") {
+      echo $num1 + $num2;
+    } elseif ( $op == "-") {
+      echo $num1 - $num2;
+    } elseif ( $op == "*") {
+      echo $num1 * $num2;
+    } elseif ( $op == "/") {
+      echo $num1 / $num2;
+    } else {
+      echo "Invalid operator";
+    }
+
+  ?>
 </body>
 </html>
