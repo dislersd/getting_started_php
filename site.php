6cb8bf7d2d561feaf5932b3aee0056c44e4f3254
dislersd <dislersd@gmail.com>
2020-04-22 20:58:07 -0700

    learning variables

---
+++ site.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <?php
+    $characterName = "Tom";
+    $characterAge = 35;
+    
+    echo "<h1> Dylan's Site </h1>";
+    echo "There once was a man named $characterName <br>";
+    echo "He was $characterAge years old <br>";
+    echo "He really liked the name $characterName <br>";
+    echo "He was happy to be $characterAge years old <br>";
+  ?>
+</body>
+</html>
