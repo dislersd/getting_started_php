21aea38bc292e732ae687c3c966b500abda9c89e
dislersd <dislersd@gmail.com>
2020-04-23 14:26:21 -0700

    post method

---
+++ getpost.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <form action="getpost.php" method="post">
+    Password: <input type="password" name="password"> <br>
+    <input type="submit">
+  </form>
+
+  <?php
+    echo $_POST["password"];
+  ?>
+</body>
+</html>
