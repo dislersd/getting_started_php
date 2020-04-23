2f281e04e8d69da325a38ffbbec131a523b8b750
dislersd <dislersd@gmail.com>
2020-04-23 15:43:25 -0700

    functions

--- functions.php
+++ functions.php
@@ -14,5 +14,16 @@
     return $num ** 3;
   }
 
-  echo cube(4);
+  echo cube(4), "<br>";
+
+
+  $add = function ($a, $b) {
+    return $a + $b;
+  };
+
+  function hof ($x, $y, $func) {
+    return $func($x, $y);
+  };
+
+  echo hof(3,10,$add);
 ?>
